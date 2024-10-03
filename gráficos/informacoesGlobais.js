c03bf5575afee92696047f8c9f23aec39aa9ce6f
kettlyncabral <105360394+kettlyncabral@users.noreply.github.com>
2024-10-03 10:23:53 -0300

    ok

--- gráficos/informacoesGlobais.js
+++ gráficos/informacoesGlobais.js
@@ -1,5 +1,6 @@
 const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/dados-globais.json'
-
-async function visualizarInformacoesGlobais
-    
+ 
+async function visualizarInformacoesGlobais() {
+    const res = await fetch 
 }
+    
