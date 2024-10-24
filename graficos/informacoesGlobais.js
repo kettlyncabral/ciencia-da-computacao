fdd61beff3c9e1ec60e3eb5b1447a554f0032c18
kettlyncabral <105360394+kettlyncabral@users.noreply.github.com>
2024-10-24 10:21:49 -0300

    ok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -4,10 +4,13 @@ async function visualizarInformacoes (){
     const res = await fetch (url)
     const dados = await res.json()
     const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
+    const pessoasNoMundo = (dados.total_pessoas_conectadas)/1e9
+    const horas = parseInt(dados.tempo_medio)
+
 
 
     const paragrafo = document.createElement('p')
-    paragrafo.innerHTML = `você sabia que o mundo tem ${dados.total_pessoas_mundo} de pessoas e que aproximadamente ${dados.total_pessoas_conectadas} estão conectadas em alguma rede social e passam em média ${dados.tempo_medio} horas conectadas.`
+    paragrafo.innerHTML = `você sabia que o mundo tem ${pessoasNoMundo} de pessoas e que aproximadamente ${pessoasConectadas} estão conectadas em alguma rede social e passam em média ${horas} horas conectadas.`
     
     const container = document.getElementsById('graficos-container') 
     container.appendChild(paragrafo)
