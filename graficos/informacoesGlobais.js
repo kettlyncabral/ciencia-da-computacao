c0f2e26e5f4c3380af7d1da86ea7d5d6f77c8812
kettlyncabral <105360394+kettlyncabral@users.noreply.github.com>
2024-10-24 10:32:02 -0300

    okok

--- graficos/informacoesGlobais.js
+++ graficos/informacoesGlobais.js
@@ -6,6 +6,8 @@ async function visualizarInformacoes (){
     const pessoasConectadas = (dados.total_pessoas_conectadas) / 1e9
     const pessoasNoMundo = (dados.total_pessoas_conectadas)/1e9
     const horas = parseInt(dados.tempo_medio)
+    const minutos = Math.round((dados.tempo_medio - horas * 100))
+    conts porcetagemConectada = ((pessoasConectadas / total_pessoas_conectadas) * 100).toFixed(2) 
 
 
 
